cabc46fe3c81eb5e3e90f1927621a3f5a69317a8
yanzhonghuashu806 <1433786254@qq.com>
2026-04-08 22:07:41 +0800

    编辑员工账号业务代码开发

--- takeout-server/src/main/java/com/takeout/controller/admin/EmployeeController.java
+++ takeout-server/src/main/java/com/takeout/controller/admin/EmployeeController.java
@@ -109,4 +109,26 @@ public class EmployeeController {
         employeeService.startOrStop(status,id);
         return Result.success();
     }
+
+    /*
+    * 根据id查询员工信息
+    * */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息接口")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息：{}",id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /*
+    * 编辑员工信息
+    * */
+    @PutMapping
+    @ApiOperation("编辑员工信息接口")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息：{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
--- takeout-server/src/main/java/com/takeout/service/impl/EmployeeServiceImpl.java
+++ takeout-server/src/main/java/com/takeout/service/impl/EmployeeServiceImpl.java
@@ -130,4 +130,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.update(employee);
     }
 
+    /*
+    * 根据id查询员工信息
+    * */
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");
+
+        return employee;
+    }
+
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.update(employee);
+    }
+
 }
