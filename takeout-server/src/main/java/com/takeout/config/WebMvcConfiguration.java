13ce1cd55057c14c04361a7c27e00d3cd4b15f99
yanzhonghuashu806 <1433786254@qq.com>
2026-04-13 21:40:15 +0800

    店铺营业状态设置业务代码开发

--- takeout-server/src/main/java/com/takeout/config/WebMvcConfiguration.java
+++ takeout-server/src/main/java/com/takeout/config/WebMvcConfiguration.java
@@ -47,7 +47,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @return
      */
     @Bean
-    public Docket docket() {
+    public Docket docket1() {
         log.info("准备生成接口文档...");
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("同城外卖项目接口文档")
@@ -55,9 +55,28 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .description("同城外卖项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理端接口")
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.takeout.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.takeout.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    @Bean
+    public Docket docket2() {
+        log.info("准备生成接口文档...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("同城外卖项目接口文档")
+                .version("2.0")
+                .description("同城外卖项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.takeout.controller.user"))
                 .paths(PathSelectors.any())
                 .build();
         return docket;
